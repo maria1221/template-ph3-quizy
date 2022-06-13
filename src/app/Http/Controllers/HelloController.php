931084545e825bcc3353e719b9eaade82626ec10
Maria Semba <moliaiqianyu@gmail.com>
2022-06-13 17:10:04 +0900

    シングルアクションコントローラー

--- src/app/Http/Controllers/HelloController.php
+++ src/app/Http/Controllers/HelloController.php
@@ -6,24 +6,87 @@ use Illuminate\Http\Request;
 
 // HelloControllerクラスの中にIndexというメソッドを追加。アクションとして使われるメソッド
 // アクションメソッド 引数を持たないメソッドとして用意。returnでHTMLのソースコードwpぁ絵師　
-class HelloController extends Controller
-{
-    public function index() {
-        return <<<EOF
-<html>
-<head>
-<title>Hello/Index</title>
+// class HelloController extends Controller
+// {
+//     public function index($id='noname', $pass='unknown') {
+//         return <<<EOF
+// <html>
+// <head>
+// <title>Hello/Index</title>
+// <style>
+// body {font-size:16pt; color:#999; }
+// h1{ font-size:100px; text-align:right; color:#eee;
+// margin: -40px 0px -50px 0px; }
+// </style>
+// <body>
+//     <h1>Index</h1>
+//     <p>これは、Helloコントローラーのindexアクションです。</p>
+//     <ul>
+//         <li>ID: ($id)</li>
+//         <li>Pass: ($pass)</li>
+//     </ul>
+// </body>
+// </head>
+// </html>
+// EOF;
+// }
+// }
+global $head, $style, $body, $end;
+$head = '<html><head>';
+$style = <<<EOF
 <style>
 body {font-size:16pt; color:#999; }
 h1{ font-size:100px; text-align:right; color:#eee;
 margin: -40px 0px -50px 0px; }
 </style>
-<body>
-    <h1>Index</h1>
-    <p>これは、Helloコントローラーのindexアクションです。</p>
-</body>
-</head>
-</html>
 EOF;
+
+$body = '</head><body>';
+$end = '</body></html>';
+function tag($tag, $txt) {
+    return "<{$tag}>" . $txt . "</{$tag}>";
 }
+
+// class HelloController extends Controller
+// {
+//     public function index() {
+//         global $head, $style, $body, $end;
+
+//         $html = $head . tag('title','Hello/Index') . $style . 
+//         $body
+//         . tag('h1','Index') . tag('p','this is Index page')
+//         . '<a href="/hello/other">go to Other page</a>'
+//         . $end;
+//         return $html;
+//     }
+//     public function other() {
+//         global $head, $style, $body, $end;
+//         $html = $head . tag('title','Hello/Other') . $style .
+//         $body
+//         . tag('h1', 'Other') . tag('p','this is Other page')
+//         . $end;
+//         return $html;
+//     }
+// }
+
+class HelloController extends Controller 
+{
+    public function __invoke() {
+        return <<<EOF
+        <html>
+        <head>
+        <title>Hello/Index</title>
+        <style>
+        body {font-size:16pt; color:#999; }
+        h1{ font-size:100px; text-align:right; color:#eee;
+        margin: -40px 0px -50px 0px; }
+        </style>
+        <body>
+            <h1>Single Action</h1>
+            <p>これは、シングルアクションコントローラーのアクションです。</p>
+        </body>
+        </head>
+        </html>
+        EOF;
+    }
 }
--- src/routes/web.php
+++ src/routes/web.php
@@ -17,75 +17,10 @@
 Route::get('/', function () {
     return view('welcome');
 });
-// helloと表示する
-// Route::get('hello', function() {
-//     return '<html><body><h1>Hello</h1><p>This is sample page.</p></body></html>';
-// });
-
-// $html = <<<EOF
-// <html>
-// <head>
-// <title>hello</title>
-// <style>
-// body {font-size:16pt; color:#999; }
-// h1 { font-size:100pt; text-align:right; color: #eee;
-//     margin:-40px 0px -50px 0px; }
-// </style>
-// </head>
-// <body>
-//     <h1>Hello</h1>
-//     <p>This is sample page.</p>
-//     <p>これは、サンプルで作ったページです。</p>
-// </body>
-// </html>
-// EOF;
-
-// Route::get('hello', function () use ($html) {
-    // return $html;
-// });
-
-//必須パラメータ パラメータを指定しないでアクセスするとエラーを起こす
-// Route::get('hello/{msg}', function ($msg) {
-//     $html = <<<EOF
-//     <html>
-//     <head>
-//         <title>hello</title>
-//             <style>
-//                 body {font-size:16pt; color:#999; }
-//                 h1 { font-size:100pt; text-align:right; color: #eee;
-//                     margin:-40px 0px -50px 0px; }
-//             </style>
-//     </head>
-//     <body>
-//         <h1>Hello</h1>
-//         <p>{$msg}</p>
-//         <p>これは、サンプルで作ったページです。</p>
-//     </body>
-//     </html>
-// EOF;
-//     return $html;
-// });
-// 任意パラメータ 任意に付けられるパラメータ。パラメータ名?
-// Route::get('hello/{msg?}', function ($msg='no message.') {
-//     $html = <<<EOF
-//     <html>
-//     <head>
-//         <title>hello</title>
-//             <style>
-//                 body {font-size:16pt; color:#999; }
-//                 h1 { font-size:100pt; text-align:right; color: #eee;
-//                     margin:-40px 0px -50px 0px; }
-//             </style>
-//     </head>
-//     <body>
-//         <h1>Hello</h1>
-//         <p>{$msg}</p>
-//         <p>これは、サンプルで作ったページです。</p>
-//     </body>
-//     </html>
-// EOF;
-//     return $html;
-// });
 
 // 呼び出すコントローラー@アクション名
-Route::get('hello', 'HelloController@index');
+// Route::get('hello', 'HelloController@index');
+// Route::get('hello/{id?}/{pass?}', 'HelloController@index');
+// Route::get('hello', 'HelloController@index');
+// Route::get('hello/other', 'HelloController@other');
+Route::get('hello', 'HelloController');
