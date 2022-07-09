0c2ea8c09ecedb7cd19a1cc2ede792108bfea596
Maria Semba <moliaiqianyu@gmail.com>
2022-07-09 22:44:36 +0900

    配列の記述をコントローラーに書いた

--- src/app/Http/Controllers/QuizController.php
+++ src/app/Http/Controllers/QuizController.php
@@ -10,6 +10,19 @@ class QuizController extends Controller
     return view('quiz.index');
     }
     public function quiz($id) {
-    return view('quiz.quiz', ['id'=>$id]);
+        $choices = [  
+            1 => [  
+                ["たかなわ","こうわ","たかわ"],
+                ["かめいど","かめど","かめと"],
+                ["こうじまち","おかとまち","かゆまち"],
+            ],
+            2 => [
+                ["むかいなだ", "むこうひら", "むきひら"],
+                ["みつぎ", "おしらべ", "みよし"],
+                ["きやま", "ぎんざん", "かなやま"],
+            ]
+    ];
+    return view('quiz.quiz',  compact('id', "choices['$id']"), );
     }
+
 }
