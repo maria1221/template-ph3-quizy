20bb458f48be45716099119f6f2f881905b837e8
Maria Semba <moliaiqianyu@gmail.com>
2022-08-07 00:54:23 +0900

    エラー解消した

--- src/app/Http/Controllers/QuizController.php
+++ src/app/Http/Controllers/QuizController.php
@@ -9,20 +9,8 @@ class QuizController extends Controller
     public function index() {
     return view('quiz.index');
     }
-    public function quiz($id) {
-        $choices = [  
-            1 => [  
-                ["たかなわ","こうわ","たかわ"],
-                ["かめいど","かめど","かめと"],
-                ["こうじまち","おかとまち","かゆまち"],
-            ],
-            2 => [
-                ["むかいなだ", "むこうひら", "むきひら"],
-                ["みつぎ", "おしらべ", "みよし"],
-                ["きやま", "ぎんざん", "かなやま"],
-            ]
-    ];
-    return view('quiz.quiz',  compact('id', "choices['$id']"), );
-    }
 
+    public function quiz($id) {
+            return view('quiz.quiz', ['id'=>$id]);
+            }
 }
