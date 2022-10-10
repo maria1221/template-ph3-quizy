42156e849cb27da72f98b9a5e92cae073a8b1f4d
Maria Semba <moliaiqianyu@gmail.com>
2022-10-11 02:39:15 +0900

    問題の選択肢が表示されるようにした

--- src/resources/views/quiz/quiz.blade.php
+++ src/resources/views/quiz/quiz.blade.php
@@ -9,30 +9,41 @@
   <link rel="stylesheet" href="{{ asset('css/quiz.css') }}">
 </head>
 <body>
+  <h1>ガチで
+    @foreach($big_questions as $big_question)  
+      @php
+        print_r($big_question->prefectures_name);
+      @endphp
+    @endforeach
+    の人しか解けない！＃
+    @php
+      print_r($big_question->prefectures_name);
+    @endphp
+    の難読地名クイズ
+  </h1>
+  @foreach($questions as $index =>$question)
+  <div class="question">
+    <h2 class="underline"> 
+      @php 
+        echo $index+1  . ". この地名はなんて読む？"; 
+      @endphp 
+    </h2> 
+    <img class="question_img" src="{{ asset('img/' . $question->prefectures_id . '-' .  ($index+1)
+    .
+    '.png') }}" alt="問いとなる地名の画像"/>
+    <ul>
+      {{-- choicesテーブルのquestion_idが問題のidと同じであればいい
+        →問題のid=questionsテーブルのid --}}
+      @foreach($choices->where('question_id', $question->id) as $choice)
+        <li id="correct" class="choice" >
+          {{$choice->choice}}
+        </li>
+      @endforeach
+    </ul>
+  </div>
+  @endforeach
 
 
-<h1>ガチで
-@foreach($questions as $question)  
-@php
-print_r($question->prefectures_name);
-@endphp
-@endforeach
-の人しか解けない！＃
-@php
-print_r($question->prefectures_name);
-@endphp
-の難読地名クイズ
-</h1>
-@foreach($questions as $index =>$question)
-<h2 class="underline"> 
-  @php 
-  echo $index+1  . ". この地名はなんて読む？"; 
-  @endphp 
-</h2> 
-<img class="question_img" src="{{ asset('img/' . $question->prefectures_id . '-' .  ($index+1)
-.
-'.png') }}" alt="問いとなる地名の画像"/>
-@endforeach
-</ul>
+  
 </body>
 </html>
